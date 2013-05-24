972dd73adc1fc578b5ee8cfac8192992b0fb0142
Michael Epstein <michael.epstein.10@ucl.ac.uk>
2013-05-24 18:30:43 +0100

    variable limits for profile param testing

--- Tools/parallelProfileLikelihood.m
+++ Tools/parallelProfileLikelihood.m
@@ -1,6 +1,6 @@
 %('Samples/Simulations/1985/test_1.scn','Tools/Mechanisms/model_params_CS 1985_2.mat',100,1)
 
-parameter_keys=[1,2,3,4,5,6];%,11,13,14]; %keys for 1985 model
+parameter_keys=[1,2,3,4,5,6,11,13,14]; %keys for 1985 model
 
 points=2;
 param_no=length(parameter_keys);
@@ -11,7 +11,7 @@ min=[500,20000,2000,10,20000,50];
 max=[4000,80000,10000,200,100000,500];
 
 
-parfor i=1:length(parameter_keys)
+for i=1:length(parameter_keys)
     [profiles(i,:,:),profile_likelihoods(i,:)] = profileLikelihood('Samples/Simulations/1985/test_1.scn','Tools/Mechanisms/model_params_CS 1985_2.mat',points,parameter_keys(i),min(i),max(i));
 end
 
