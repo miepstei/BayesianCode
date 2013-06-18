90e53933a8cf85dd2454fbe9dc94eb47f15d7cfb
Michael Epstein <michael.epstein.10@ucl.ac.uk>
2013-06-18 11:52:49 +0100

    Updated concentration input

--- Tools/simulate_data.m
+++ Tools/simulate_data.m
@@ -1,9 +1,10 @@
 function simulate_data(simFile,mechParams,intervals)
     %2003 generative params
+    load(mechParams)
     generativeMec=ModelSetup(mechParams);%'/Volumes/Users/Dropbox/Academic/PhD/Code/git-repo/Testing/Results/matlab_params_CS 1985_2.mat');
 
     %generate  intervals
-    datasim=generate(generativeMec,10000*60*15,3e-08,intervals);
+    datasim=generate(generativeMec,10000*60*15,concentration,intervals);
     handle=fopen(simFile,'w','n','UTF-8');
     DataController.write_scn_file(handle,datasim);
 end
