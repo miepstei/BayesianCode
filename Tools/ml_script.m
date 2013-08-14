0653116d8b786c07f2583f277b1220f1db3c8f31
michaelepstein <michael.epstein.10@ucl.ac.uk>
2013-08-14 16:24:27 +0100

    Updated script for dcprogs

--- Tools/ml_script.m
+++ Tools/ml_script.m
@@ -11,33 +11,7 @@ function ml_script(outfile,random_starts,datafile,paramsFile)
     parameter_keys=[1,2,3,4,5,6,11,13,14]; %keys for 1985 model, 9 free params
     min_range=[500,20000,2000,10,20000,50,500,5000,200000000];
     max_range=[24000,200000,10000,200,100000,500,3000,15000,600000000];
-    %datafile='/Volumes/Users/Dropbox/Academic/PhD/Code/git-repo/Samples/Simulations/20000/test_1.scn';
-    %paramsFile='/Volumes/Users/Dropbox/Academic/PhD/Code/git-repo/Tools/Mechanisms/model_params_CS 1985_2.mat';
-
-    load(paramsFile);
-
-    [~,data]=DataController.read_scn_file(datafile);
-    data.intervals=data.intervals/1000;
-
-    test_params.mechanism=ModelSetup(paramsFile); %'/Volumes/Users/Dropbox/Academic/PhD/Code/git-repo/Testing/Results/matlab_params_CS 1985_4.mat');
-    test_params.islogspace=true;
-    test_params.debugOn=true;
-    test_params.tres = tres; 
-    test_params.tcrit = tcrit; %separation of time between bursts  
-    test_params.isCHS = 1; % use CHS vectors
-    test_params.data = data;
-    test_params.conc = concentration;
-
-    %preprocess data and apply resolution
-    resolvedData = RecordingManipulator.imposeResolution(data,test_params.tres);
-    bursts = RecordingManipulator.getBursts(resolvedData,test_params.tcrit);
-    test_params.bursts=bursts;
-
-    %prepare the dataset for likelihood calc
-    lik = ExactLikelihood();
-    [test_params.open_times,test_params.closed_times,test_params.withinburst_count,test_params.l_openings]=lik.calculate_burst_parameters(bursts);
-    clear lik; 
-    
+   
     fitted_params=zeros(random_starts,length(parameter_keys));
     fitted_lik = zeros(random_starts,1);
     fitted_iter = zeros(random_starts,1);
@@ -54,19 +28,10 @@ function ml_script(outfile,random_starts,datafile,paramsFile)
             rand_params(parameter_keys(j)) = randi([min_range(j) max_range(j)],1);
         end
         start_params(i,:)=cell2mat(rand_params.values);
-
-        %set the params on the mechanism
-        test_params.mechanism.setParameters(rand_params);
-
-        lik = ExactLikelihood();
-        [test_params.open_times,test_params.closed_times,test_params.withinburst_count,test_params.l_openings]=lik.calculate_burst_parameters(bursts);
-        init_params=test_params.mechanism.getParameters(true);
-        splx=Simplex();
-
         a=tic;
         try
-            [min_function_value,min_parameters,iter,rejigs,errors,~]=splx.run_simplex(lik,init_params,test_params);
-            fitted_params(i,:) = cell2mat(min_parameters.values);
+            [min_function_value,fittedRates,min_parameters,iter,rejigs,errors,~] = fit_simplex( datafile,paramsFile,1,rand_params );
+            fitted_params(i,:) = cell2mat(fittedRates.values);
             fitted_lik(i) = min_function_value;
             fitted_iter(i) = iter;
             fitted_rejigs(i) = rejigs;
